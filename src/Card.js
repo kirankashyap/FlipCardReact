62fd5610c2fd528e876a58db9eda08333e4b17df
kirankashyap <kiran.kashyap.ds@gmail.com>
2019-10-16 09:21:38 +0530

    add state

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Card extends React.Component{
 
   render(){
-    return <div>goo</div>
+    return <div>{this.props.title}</div>
     ;
   }
 
--- src/HomePageComponent.js
+++ src/HomePageComponent.js
@@ -6,23 +6,49 @@ import ReactDOM from 'react-dom';
 
 class HomePageComponent extends React.Component{
 
+constructor(){
+  super();
+  this.state = {
+    cards: ['auto','bike','car']
+  }
+}
 
+renderCards(){
+  const elements = this.state.cards;
+
+  const items = []
+
+  for (const [index, value] of elements.entries()) {
+    items.push(<Card title={value} />)
+  }
+
+  return items;
+}
+
+
+addQuestion(){
+  console.log(this.getState());
+
+}
 
 render(){
-  return <div>
+  return (<div>
   <h1> Flipcard </h1>
   Question: <input type="text" id="questionText"/>
   <button id="addButton" onClick={this.addQuestion}>Add</button>
 
   <table id="cardsTable">
-
+    <tr>
+      <td>
+      {this.renderCards()}
+      </td>
+    </tr>
 
   </table>
 
-  </div>;
+</div>);
 }
-addQuestion(){
-}
+
 
 }
 
