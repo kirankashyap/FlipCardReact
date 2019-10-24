bb52d3ed503bde127c9d6ddcbd26e9e9e54621a4
kirankashyap <kiran.kashyap.ds@gmail.com>
2019-10-24 10:05:02 +0530

    added api

---
+++ api/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+//import db from './db/db';
+// Set up the express app
+const app = express();
+
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'confdemo',
+  password: 'tdd',
+  port: 5432,
+})
+
+// get all todos
+app.get('/api/questions', (req, res) => {
+	 pool.query('SELECT * FROM questions', (error, results) => {
+    if (error) {
+      throw error
+    }
+    res.status(200).json(results.rows)
+  })
+  
+  
+});
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`)
+});
