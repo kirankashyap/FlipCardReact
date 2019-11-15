2b52a8e2e232a1987de4a60950f687baf3e39009
kirankashyap <kiran.kashyap.ds@gmail.com>
2019-11-15 11:33:47 +0530

    setup and teardown for API tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,9 +1,37 @@
 const request = require('supertest');
 let app = require('../app');
 const assert = require('assert');
+const dbConnector = require("../DBConnector");
+
+
 
 
 describe('API Tests', () => {
+
+  before(function(done){
+    //delete all rows in questions table
+    var queryToRun = "delete from questions";
+    dbConnector.query(queryToRun, (error, results) => {
+      assert(!error);
+    });
+
+    //load sample data
+    queryToRun = "insert into questions (title) values ('q1'); insert into questions (title) values ('q2'); insert into questions (title) values ('q3');"
+    dbConnector.query(queryToRun, (error, results) => {
+      done();
+    });
+  })
+
+  after(function(done){
+    //delete all rows in questions table
+    var queryToRun = "delete from questions";
+    dbConnector.query(queryToRun, (error, results) => {
+      assert(!error);
+      done();
+    });
+  })
+
+
   it('should be able to get status 200', function (done) {
       request(app)
             .get('/api/questions')
@@ -11,11 +39,11 @@ describe('API Tests', () => {
 
   })
 
-  it('should get >0 questions', function (done) {
+  it('should get 3 questions', function (done) {
       request(app)
             .get('/api/questions')
             .then(response => {
-                assert(response.body.length !== 0)
+                assert(response.body.length==3)
                 done();
               })
   })
@@ -24,8 +52,17 @@ describe('API Tests', () => {
       request(app)
             .get('/api/questions')
             .then(response => {
-                assert(response.body[0].id===1)
-                assert(response.body[1].id===2)
+                assert(response.body[0].id < response.body[1].id )
+                done();
+              })
+  })
+
+  it('should be able to get correct titles in questions', function (done) {
+      request(app)
+            .get('/api/questions')
+            .then(response => {
+                assert(response.body[0].title === 'q1')
+                assert(response.body[1].title === 'q2')
                 done();
               })
   })
